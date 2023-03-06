15b1142ee2f2ddf03edba8a6083aac4972042f6a
theprostonikol <mykola.tronets200116@gmail.com>
2023-03-06 19:47:27 +0200

    EmployeeProject

---
+++ src/Employee.java
@@ -0,0 +1,16 @@
+public class Employee {
+    private String name;
+    private String position;
+    private String email;
+     private String phone;
+    private int age;
+
+    public Employee(String name,String position,String email, String phone, int age) {
+        this.name = name;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.age = age;
+
+    }
+}
---
+++ src/Сar.java
@@ -0,0 +1,16 @@
+public class Сar{
+    public void start() {
+        startElectricity();
+        startCommand();
+        startFuelSystem();
+    }
+    private void startElectricity() {
+
+    }
+    private void startCommand() {
+
+    }
+    private void startFuelSystem() {
+
+    }
+}
